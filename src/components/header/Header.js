988f8d8e577a0f1b38e9bdd1aa3b06551e673a69
imar26 <aadeshranderia26@gmail.com>
2018-06-09 02:21:50 -0400

    added header/footer

---
+++ src/components/header/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+    render() {
+        return(
+            <header>
+                <div>
+                    <h3>Twitter Webapp - React JS | Node.js</h3>
+                    <p>A simple webapp to display tweets with one or more hashtags</p>
+                </div>
+            </header>
+        )
+    }
+}
+
+export default Header;
