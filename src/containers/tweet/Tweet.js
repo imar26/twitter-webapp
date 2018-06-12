0bbeca7a22c206e42b0b9745478e217e131cf27e
imar26 <aadeshranderia26@gmail.com>
2018-06-12 05:32:54 -0400

    retweets displayed as normal tweets

--- src/containers/tweet/Tweet.js
+++ src/containers/tweet/Tweet.js
@@ -2,35 +2,35 @@
 import React, { Component } from 'react';
 
 class Tweet extends Component {
+    tweetText = '';
     constructor(props) {
         super(props);
 
-        console.log(this.props.tweet);
+        // Check if the tweet is a retweet
+        if(this.props.tweet.retweeted_status === undefined) {            
+            // Display normal tweet
+            this.tweetText = this.props.tweet.full_text;
+        } else {
+            // Display original tweet which has been retweeted (Note: Not using retweet because the text is truncated)
+            this.tweetText = this.props.tweet.retweeted_status.full_text;
+        }
 
-        // created_at
-        // favorite_count
-        // retweet_count
-        // text
-        // user.name
-        // user.screen_name
+        console.log(this.props.tweet);
     }
 
     render() {
         return(
             <div className="clearfix tweet">
                 <div className="left-section">
-                    <img src={this.props.tweet.user.profile_image_url_https.replace('_normal', '')} />
+                    <img src={this.props.tweet.user.profile_image_url_https.replace('_normal', '')} alt={this.props.tweet.user.name + '-img'} />
                 </div>
                 <div className="right-section">
                     <h4>{this.props.tweet.user.name} <span>@{this.props.tweet.user.screen_name}</span></h4>
-                    <p>{this.props.tweet.full_text}</p>
-                    <span>Total Retweets: {this.props.tweet.retweet_count}</span>
-                    <span>Total Favourites: {this.props.tweet.favorite_count}</span>
-                    <span>Tweeted at: {this.props.tweet.created_at}</span>
+                    <p>{this.tweetText}</p>
+                    <span><strong>Total Retweets:</strong> {this.props.tweet.retweet_count}</span>
+                    <span><strong>Total Favourites:</strong> {this.props.tweet.favorite_count}</span>
+                    <span><strong>Tweeted at:</strong> {this.props.tweet.created_at}</span>
                 </div>
-                {/* {
-                    this.props.tweet.retweeted_status.full_text
-                } */}
             </div>
         )
     }
--- src/containers/tweetsList/TweetsList.js
+++ src/containers/tweetsList/TweetsList.js
@@ -5,10 +5,6 @@ import React, { Component } from 'react';
 import Tweet from '../tweet/Tweet';
 
 class TweetsList extends Component {
-    constructor(props) {
-        super(props);               
-    }
-
     sortTweets() {
         let sortValue = this.refs.sortValue.value;
 
